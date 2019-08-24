57333c3fc38df784ffc0f529073c5da6e258f5fd
MarcPuigdemont <marcpuigdemontriera@gmail.com>
2019-08-24 12:29:25 +0200

    Create simple backend server with express

--- README.md
+++ README.md
@@ -2,56 +2,77 @@
 
 This is a simple CRUD created with vue to represent a simple airline list.
 
-The project has been created with vue-cli 3. 
+The project has been created with vue-cli 3.
 
 As a style framework, BootstrapVue has been chosen as it provides a directive and component system very well suited for this example.
 
 The coding style is based on Airbnb, but since it's using eslint + @vue/eslint-config-airbnb + prettier maybe some of the rules may be off. The intention is not being 100% accurate with one coding style but to provide a consistent style over all the written code.
 
-
-
-
 ## Project setup
+
 ```
 npm install
 ```
+
 ### Server
-This project runs on a simple backend done on node and express. It should be started on a separate terminal BEFORE running the frontend.
+
+This project runs on a simple Backend done on node and express. It should be started on a separate terminal BEFORE running the Frontend.
 
 Use:
+
 ```
-node server/server.js
+npm run start-server
 ```
 
+As a design choice, the primary key or id is the combination of 'iata' and 'name' properties, as it is as close as possible to the supplied data. This will force some logic to be on the Frontend, which is good for the sake of the example as Frontend is the main goal of this project.
+
+The API offers:
+
+- GET /airlines to get a list of all airlines
+- POST /airline with a json body with the desired data to create a new airline
+- PUT /airline with a json body with the desired modified data of an existing airline to update it
+- DELETE /airline with a json body with 'iata' and 'name' properties to delete the desired airline
+
+Finally to reset to the default airlines from the template, use:
+
+- GET /reset
+
 ### Compiles and hot-reloads for development
+
 ```
 npm run serve
 ```
 
 ### Compiles and minifies for production
+
 ```
 npm run build
 ```
 
 ### Run your tests
+
 ```
 npm run test
 ```
 
 ### Lints and fixes files
+
 ```
 npm run lint
 ```
 
 ### Run your end-to-end tests
+
 ```
 npm run test:e2e
 ```
 
 ### Run your unit tests
+
 ```
 npm run test:unit
 ```
 
 ### Customize configuration
+
 See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ server/server.js
@@ -0,0 +1,114 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+const express = require('express');
+// eslint-disable-next-line import/no-extraneous-dependencies
+const bodyParser = require('body-parser');
+const fs = require('fs');
+
+const app = express();
+app.use(bodyParser.json());
+
+const cleanAirline = airline => {
+  const cleanAirLine = {};
+  if (airline['iata']) cleanAirLine['iata'] = airline['iata'];
+  if (airline['name']) cleanAirLine['name'] = airline['name'];
+  if (airline['primary_color']) cleanAirLine['primary_color'] = airline['primary_color'];
+  if (airline['secondary_color']) cleanAirLine['secondary_color'] = airline['secondary_color'];
+  if (airline['services']) cleanAirLine['services'] = airline['services'];
+  return cleanAirLine;
+};
+
+let airlines = [];
+const reset = () => {
+  try {
+    airlines = JSON.parse(fs.readFileSync('server/airlines-template.json', 'utf8'));
+  } catch (error) {
+    if (error.code === 'ENOENT') {
+      console.info('Template file for airlines not found. Initializing empty array');
+    } else if (error instanceof SyntaxError) {
+      console.info('Template file for airlines is corrupted. Initializing empty array');
+    } else {
+      throw error;
+    }
+  }
+};
+reset();
+
+app.get('/', (req, res) => {
+  res.send(`The primary key or id is the combination of iata and name properties. Use paths:<br>
+  GET /airlines to get a list of all airlines<br>
+  POST /airline with a json body with the desired data to create a new airline<br>
+  PUT /airline with a json body with the desired modified data of an existing airline to update it<br>
+  DELETE /airline with a json body with iata and name properties to delete the desired airline<br>
+  <br>
+  Finally to reset to the default airlines from the template, use: <br>
+  GET /reset`);
+});
+
+app.get('/airlines', (req, res) => {
+  res.json(airlines);
+});
+app.get('/reset', (req, res) => {
+  reset();
+  res.json(airlines);
+});
+
+app.post('/airline', (req, res) => {
+  const data = req.body;
+  if (!data.iata || !data.name) {
+    res.status(400).send('Missing iata or name properties');
+  } else {
+    const exists = airlines.find(
+      airline => airline.iata.toLowerCase() === data.iata.toLowerCase() && airline.name.toLowerCase() === data.name.toLowerCase()
+    );
+    if (exists) {
+      res.status(400).send('Trying to create an already existing airline, please use PUT method instead');
+    } else {
+      const airline = cleanAirline(req.body);
+
+      airlines.push(airline);
+      res.send('POST successful to /airline');
+    }
+  }
+});
+
+app.put('/airline', (req, res) => {
+  const data = req.body;
+  if (!data.iata || !data.name) {
+    res.status(400).send('Missing iata or name properties');
+  } else {
+    const foundAirline = airlines.find(
+      airline => airline.iata.toLowerCase() === data.iata.toLowerCase() && airline.name.toLowerCase() === data.name.toLowerCase()
+    );
+    if (foundAirline) {
+      const airline = cleanAirline(req.body);
+
+      Object.assign(foundAirline, airline);
+      res.send('PUT successful to /airline');
+    } else {
+      res.status(400).send('Trying to update an unexisting airline, please use POST method if you want to create it instead');
+    }
+  }
+});
+
+app.delete('/airline', (req, res) => {
+  const data = req.body;
+  if (!data.iata || !data.name) {
+    res.status(400).send('Missing iata or name properties');
+  } else {
+    const foundAirline = airlines.find(
+      airline => airline.iata.toLowerCase() === data.iata.toLowerCase() && airline.name.toLowerCase() === data.name.toLowerCase()
+    );
+    if (foundAirline) {
+      airlines = airlines.filter(
+        airline => airline.iata.toLowerCase() !== foundAirline.iata.toLowerCase() && airline.name.toLowerCase() !== foundAirline.name.toLowerCase()
+      );
+      res.send('DELETE successful to /airline');
+    } else {
+      res.status(400).send('Trying to delete an unexisting airline');
+    }
+  }
+});
+
+app.listen(3000, () => {
+  console.log('Example app listening on port 3000!');
+});
