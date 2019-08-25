e0217e834c456178fe4baf946a65d6773ef963a7
MarcPuigdemont <marcpuigdemontriera@gmail.com>
2019-08-25 16:38:43 +0200

    Add headers to the airline list

--- src/components/AirlineList.vue
+++ src/components/AirlineList.vue
@@ -1,5 +1,12 @@
 <template>
   <b-list-group class="w-100">
+    <b-row no-gutters class="pl-3 font-weight-bold">
+      <b-col>IATA</b-col>
+      <b-col>Name</b-col>
+      <b-col>Primary Color</b-col>
+      <b-col>Secondary Color</b-col>
+      <b-col>Available Services</b-col>
+    </b-row>
     <AirlineListItem v-for="(airline, index) in filteredAirlines" :key="index" :airline="airline" @edit="edit" />
   </b-list-group>
 </template>
